6ede3bfb178e73b63be7462b5767caa924fa58b2
Yashas P <cyberjeet13@gmail.com>
2025-07-23 12:45:21 +0000

    SupplySync SupplySync Firebase AI Logic All apps AI monitoring Settings

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, onSnapshot, doc, deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TODO",
-  authDomain: "TODO",
-  projectId: "TODO",
-  storageBucket: "TODO",
-  messagingSenderId: "TODO",
-  appId: "TODO"
+  apiKey: "AIzaSyBLtMzqEa_y81OJNCRbnxNPems9NLMxRgI",
+  authDomain: "supplysync-m929o.firebaseapp.com",
+  projectId: "supplysync-m929o",
+  storageBucket: "supplysync-m929o.appspot.com",
+  messagingSenderId: "242460684670",
+  appId: "1:242460684670:web:efbbdc69e9266a2a98832f"
 };
 
 const app = initializeApp(firebaseConfig);
